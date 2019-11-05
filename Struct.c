763f2c9c637e5981d9553a9c29021a0a06004ac6
rdghenrique94 <54558924+rdghenrique94@users.noreply.github.com>
2019-11-04 23:04:29 -0300

    Testando as estruturas

--- Struct.c
+++ Struct.c
@@ -1,19 +1,61 @@
 #include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+struct horario
+{
+    int horas;
+    int minutos;
+    int segundos;
+};
 
 int main (void){
+setlocale(LC_ALL,"portuguese");
 
-    struct horario
+//DEFININDO ESTRUTURA
 
+    /*struct horario //tipo da estrutura, nome da estrutura
+{
       int horas;
       int minutos;
       int segundos;
+      double teste;
+      char letra;
 
-  };
+};
 
-    struct horario agora;
+//DECLARANDO ESTRUTURA
 
-    agora.horas = 15;
-    agora.minutos = 17;
-    agora.segundos = 30;
+    struct horario agora; //NOME DA ESTRUTURA
+//VARIAVES DA ESTRUTURA ------- NOME DA ESTRUTURA MAIS VARIAVEL
+    agora.horas;
+    printf("DIGITE AS HORAS\n");
+    scanf("%i", &agora.horas);
+    agora.minutos;
+    printf("DIGITE OS MINUTOS\n");
+    scanf("%i", &agora.minutos);
+    agora.segundos;
+    printf("DIGITE OS SEGUNDOS\n");
+    scanf("%i", &agora.segundos);
 
-    printf("%i%i%i\n",agora.horas, agora.minutos, agora.segundos );
+    printf("São exatos: %i Horas: %i Minutos e : %i Segundos  \n ", agora.horas, agora.minutos, agora.segundos );
+
+
+    struct horario depois;
+
+    depois.horas = agora.horas +10;
+    depois.minutos = agora.minutos *2;
+    depois.segundos = agora.segundos -5;
+    depois.teste = 50.55 / 123;
+    depois.letra = 'r';
+
+
+    printf("Fuso Horario: %i Horas :%i Minutos :%i Segundos :\n", depois.horas, depois.minutos, depois.segundos);
+    printf("O valor da operação é: %f\n", depois.teste);
+    printf("Valor referente ao %c\n", depois.letra);
+}*/
+
+
+
+    return x;
+}
